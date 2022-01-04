6cce0028700ecdcc2f65a1087b33f28e1f8f29ee
amrbashir <amr.bashir2015@gmail.com>
2022-01-04 12:46:55 +0200

    win: update bootstrap-windows.ps1

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -1,89 +1,95 @@
 #Requires -RunAsAdministrator
 
 if ($PSVersionTable.PSEdition -ne "Core") {
-   Write-Host "❌ Install Powershell Core first then use it to run the script." -ForegroundColor Red
+   Write-Host "❌ Install Powershell Core first using ``winget install --id Microsoft.Powershell`` then use it to run this script." -ForegroundColor Red
    exit
 }
 
-Set-ExecutionPolicy Bypass -Scope CurrentUser
-
-function RefreshPATH { $ENV:path = [System.ENVironment]::GetENVironmentVariable("Path", "Machine") + ";" + [System.ENVironment]::GetENVironmentVariable("Path", "User") }
-
-if (-Not (Get-Command scoop)) {
-   Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://get.scoop.sh")
-   RefreshPATH
+# Helper functions
+function Refresh-PATH {
+	$ENV:path = [System.ENVironment]::GetENVironmentVariable("Path", "Machine") + ";" + [System.ENVironment]::GetENVironmentVariable("Path", "User")
+}
+Function Add-PATHEntry {
+    $oldpath = (Get-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Environment" -Name PATH).path
+    $newpath = $oldpath + ";" + $args
+    Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Environment" -Name PATH -Value $newPath
+}
+Function WingetSilentInstall {
+    winget install --silent --accept-package-agreements --accept-source-agreements --source winget $args
 }
 
-scoop install git --global
-RefreshPATH
+# Remove execution restrictions
+Set-ExecutionPolicy Bypass -Scope CurrentUser
 
+# Install apps
+iwr -useb get.scoop.sh | iex
+scoop install -g 7zip git
+Refresh-PATH
 scoop bucket add spotify "https://github.com/TheRandomLabs/Scoop-Spotify.git"
 scoop bucket add nerd-fonts
 scoop bucket add extras
-
-scoop install sudo --global
-scoop install vcredist --global
-
-scoop install spotify-latest
-scoop install blockthespot
-scoop install spicetify-cli
-
-scoop install autohotkey
-scoop install rainmeter
-scoop install taskbarx
-scoop install trafficmonitor
-
-scoop install windows-terminal --global
-scoop install starship
-scoop install neovim
-scoop install bat
-scoop install ripgrep
-scoop install fd
-
-scoop install mailspring
-scoop install 7zip --global
-scoop install qbittorrent --global
-scoop install everything --global
-scoop install instant-eyedropper
-
+scoop install -g vcredist starship neovim bat ripgrep fd less qbittorrent python everything notepadplusplus
+scoop install mailspring spicetify-cli autohotkey trafficmonitor instant-eyedropper cmake fnm yarn rustup
+WingetSilentInstall ModernFlyouts
+WingetSilentInstall Vivaldi
+WingetSilentInstall VLC
+WingetSilentInstall VSCode
+WingetSilentInstall Steam
+WingetSilentInstall "Windows Terminal"
+WingetSilentInstall AltSnap
+WingetSilentInstall Discord
+WingetSilentInstall Spotify
+iwr https://get.pnpm.io/install.ps1 -useb | iex
 git clone "https://github.com/microsoft/vcpkg" "$ENV:LOCALAPPDATA/vcpkg"
 &"$ENV:LOCALAPPDATA/vcpkg/bootstrap-vcpkg.bat" -disableMetrics
-scoop install fnm
-scoop install yarn
+Refresh-PATH
 fnm install --lts
-fnm env --use-on-cd | Out-String | Invoke-Expression
-RefreshPATH
-yarn add --global pnpm
+fnm use lts-latest
+Refresh-PATH
 
-#######
-# 1. rustup - try scoop again
-# 2. vscode - try scoop again and disable auto updater in vscode
-# 3. steam - try
-# 4. ueli - we have a custom build
-# 5. modern flyouts - only from store
-# 6. vivaldi - try scoop again
-# 7. cmake - try scoop again
-# 8. vlc - try scoop again
-# 9. exa - instal from exa frok using cargo --git
+### Remaining apps:
+# - ueli - I have a custom build
+# - Visual Studio Build Tools
+# - exa -> `cargo install exa --git https://github.com/skyline75489/exa --branch chesterliu/dev/win-support`
+# - XBar or TranslucentTB
 
-scoop install FiraCode --global
-scoop install FiraCode-NF --global
+# Install Fonts
+scoop install -g FiraCode FiraCode-NF
+Refresh-PATH
 
+# Install Powershell modules
 Install-Module -Name PowerShellGet -Force
 Install-Module PSReadLine -Force -SkipPublisherCheck -AllowPrerelease
 Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+Refresh-PATH
 
+# Symlink config files
 @(
-    [PSCustomObject]@{file = "$PWD/windows/powershell.ps1"; targetDir = "$HOME\Documents\PowerShell\"; targetFile = "Microsoft.PowerShell_profile.ps1"},
-    [PSCustomObject]@{file = "$PWD/windows/windows-terminal.json"; targetDir = "$Env:LOCALAPPDATA\Microsoft\Windows Terminal\"; targetFile = "settings.json"},
-    [PSCustomObject]@{file = "$PWD/windows/spicetify.ini"; targetDir = "$HOME\.spicetify\"; targetFile = "config-xpui.ini"},
-    [PSCustomObject]@{file = "$PWD/windows/alt-snap.ini"; targetDir = "$HOME\scoop\apps\altdrag\current\"; targetFile = "AltSnap.ini"}
-    [PSCustomObject]@{file = "$PWD/shared/starship.toml"; targetDir = "$HOME\.config\"; targetFile = "starship.toml"},
-    [PSCustomObject]@{file = "$PWD/shared/.gitconfig"; targetDir = "$HOME\"; targetFile = ".gitconfig"},
-    [PSCustomObject]@{file = "$PWD/shared/neovim.vim"; targetDir = "$Env:LOCALAPPDATA\nvim\"; targetFile = "init.vim"}
+    [PSCustomObject]@{file = "$PWD/windows/powershell.ps1"; targetDir = "$HOME\Documents\PowerShell\"; targetFile = "Microsoft.PowerShell_profile.ps1"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/windows/windows-terminal.json"; targetDir = "$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"; targetFile = "settings.json"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/windows/spicetify.ini"; targetDir = "$HOME\.spicetify\"; targetFile = "config-xpui.ini"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/windows/alt-snap.ini"; targetDir = "$Env:APPDATA\AltSnap\"; targetFile = "AltSnap.ini"; symlink = $FALSE}
+    [PSCustomObject]@{file = "$PWD/shared/starship.toml"; targetDir = "$HOME\.config\"; targetFile = "starship.toml"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/shared/.gitconfig"; targetDir = "$HOME\"; targetFile = ".gitconfig"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/shared/neovim.vim"; targetDir = "$Env:LOCALAPPDATA\nvim\"; targetFile = "init.vim"; symlink = $TRUE},
+    [PSCustomObject]@{file = "$PWD/windows/traffic-monitor.ini"; targetDir = "$Env:APPDATA\TrafficMonitor\"; targetFile = "config.ini"; symlink = $TRUE}
 ) | ForEach-Object {
     New-Item -Path $_.targetDir -ItemType Directory -Force
     $target = $_.targetDir + $_.targetFile
     Remove-Item -Path $target  -Force
-    New-Item -ItemType SymbolicLink -Target $_.file -Path $target
+    if ($_.symlink -eq $TRUE) {
+        New-Item -ItemType SymbolicLink -Target $_.file -Path $target
+    } else {
+        Copy-Item $_.file $target
+    }
 }
+
+# Run post-install scripts
+&"C:\ProgramData\scoop\apps\7zip\current\install-context.reg"
+&"C:\ProgramData\scoop\apps\everything\current\install-context.reg"
+&"C:\ProgramData\scoop\apps\python\current\install-pep-514.reg"
+&"C:\ProgramData\scoop\apps\notepadplusplus\current\install-context.reg"
+Add-PATHEntry "$HOME\scoop\persist\rustup\.cargo\bin"
+Add-PATHEntry "$ENV:LOCALAPPDATA/vcpkg"
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/dribbblish-dynamic-theme/master/install.ps1" | Invoke-Expression
+spicetify backup apply
