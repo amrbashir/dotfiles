cfd8de03820601dedb9130f5296e6283967c4089
amrbashir <amr.bashir2015@gmail.com>
2022-01-07 18:52:00 +0200

    win: add rainmeter installation

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -29,7 +29,7 @@ scoop bucket add spotify "https://github.com/TheRandomLabs/Scoop-Spotify.git"
 scoop bucket add nerd-fonts
 scoop bucket add extras
 scoop install -g vcredist starship bat ripgrep fd less qbittorrent python everything notepadplusplus
-scoop install neovim mailspring spicetify-cli autohotkey trafficmonitor instant-eyedropper cmake fnm yarn rustup
+scoop install neovim mailspring spicetify-cli autohotkey trafficmonitor instant-eyedropper cmake fnm yarn rustup rainmeter
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 WingetSilentInstall ModernFlyouts
 WingetSilentInstall Vivaldi
