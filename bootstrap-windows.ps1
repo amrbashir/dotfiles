48f89c1b935b55dd03ec54fcdda461626bc7a477
amrbashir <amr.bashir2015@gmail.com>
2022-03-03 15:35:28 +0200

    win: remove rainmeter from installation

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -32,7 +32,7 @@ scoop bucket add spotify "https://github.com/TheRandomLabs/Scoop-Spotify.git"
 scoop bucket add nerd-fonts
 scoop bucket add extras
 scoop install -g uutils-coreutils vcredist starship bat ripgrep fd less qbittorrent python everything notepadplusplus
-scoop install pnpm neovim mailspring spicetify-cli autohotkey trafficmonitor instant-eyedropper cmake fnm yarn rustup rainmeter
+scoop install pnpm neovim mailspring spicetify-cli autohotkey trafficmonitor instant-eyedropper cmake fnm yarn rustup 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 WingetSilentInstall ModernFlyouts
 WingetSilentInstall Vivaldi
