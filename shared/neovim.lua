5fea91508437bce8fb3ab315a11be2a45515f654
amrbashir <amr.bashir2015@gmail.com>
2022-05-22 22:01:39 +0200

    win: fix neovim shell

--- shared/neovim.lua
+++ shared/neovim.lua
@@ -1,40 +1,43 @@
 local config = {
-  plugins = {
-    init = {
-      [ "windwp/nvim-autopairs" ] = { disable = true },
-      [ "windwp/nvim-ts-autotag" ] = { disable = true },
-      { "machakann/vim-highlightedyank" },
-      { "jghauser/mkdir.nvim" },
-      {
-        "amrbashir/nvim-docs-view",
-        opt = true,
-        cmd = { "DocsViewToggle" },
-      },
-    },
+	plugins = {
+		init = {
+			["windwp/nvim-autopairs"] = { disable = true },
+			["windwp/nvim-ts-autotag"] = { disable = true },
+			{ "machakann/vim-highlightedyank" },
+			{ "jghauser/mkdir.nvim" },
+			{
+				"amrbashir/nvim-docs-view",
+				opt = true,
+				cmd = { "DocsViewToggle" },
+			},
+		},
 
-    tree_sitter = {
-      ensure_installed = { "lua", "rust", "javascript", "typescript", "python", "bash" }
-    },
+		tree_sitter = {
+			ensure_installed = { "lua", "rust", "javascript", "typescript", "python", "bash" },
+		},
 
-    ["nvim-lsp-installer"] = {
-      ensure_installed = { "sumneko_lua", "rust_analyzer", "tsserver", "jsonls", "bashls" },
-    },
+		["nvim-lsp-installer"] = {
+			ensure_installed = { "sumneko_lua", "rust_analyzer", "tsserver", "jsonls", "bashls" },
+		},
 
-    ["neo-tree"] = {
-      window = {
-        position = "right",
-	      width = 35
-      }
-    }
-  },
+		["neo-tree"] = {
+			window = {
+				position = "right",
+				width = 35,
+			},
+		},
+	},
 }
 
 if vim.fn.has("win32") == 1 then
-  config["options"] = {
-    opt = {
-      shell = "pwsh -NoLogo"
-    }
-  }
+	config["options"] = {
+		opt = {
+			shell = "pwsh -NoLogo",
+			shellcmdflag = "-Command",
+			shellquote = '\\"',
+			shellxquote = "",
+		},
+	}
 end
 
 return config
