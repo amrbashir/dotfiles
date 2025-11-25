a211ac7863cf178230de32ab23c6478a1869469b
amrbashir <github@amrbashir.me>
2025-11-25 04:56:59 +0200

    win: add more apps

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -13,6 +13,12 @@ Function WingetSilentInstall {
     winget install --silent --accept-package-agreements --accept-source-agreements --source winget $args
 }
 
+Function InstallMyApp {
+    $appName = $args[0]
+    irm "https://github.com/amrbashir/$appName/releases/latest/download/$appName-setup.exe" -OutFile "$appName-setup.exe"
+    & "./$appName-setup.exe"
+}
+
 #################
 #  Main Script  #
 #################
@@ -20,38 +26,50 @@ Function WingetSilentInstall {
 # Install Git
 WingetSilentInstall --id Git.Git
 Add-PATHEntry "$Env:SYSTEMDRIVE\Program Files\Git\bin"
+
 Refresh-PATH
 
 # Install Scoop
 iwr -useb get.scoop.sh | iex
+
 Refresh-PATH
+
+# Add Scoop buckets
 scoop bucket add extras
 scoop bucket add nerd-fonts
 
 # Install apps
-scoop install 7zip winrar uutils-coreutils starship bat ripgrep fd neovim eza zoxide fzf
-scoop install python fnm
-sudo scoop install windowsdesktop-runtime-lts
-scoop install qbittorrent everything everything-cli instant-eyedropper mailspring ds4windows
-scoop install komorebi autohotkey trafficmonitor-lite altsnap windhawk
+scoop install 7zip winrar
+scoop install uutils-coreutils starship bat ripgrep fd neovim eza zoxide fzf
+scoop install python fnm gh cmake ninja deno nsis taplo
+scoop install bitwarden bitwarden-cli yubioath
+scoop install g-helper qbittorrent everything everything-cli instant-eyedropper mailspring ds4windows inkscape
+scoop install komorebi autohotkey trafficmonitor-lite altsnap windhawk translucenttb
 scoop install FiraCode FiraCode-NF
+sudo scoop install windowsdesktop-runtime-lts
+InstallMyApp kal
+InstallMyApp komorebi-switcher
+WingetSilentInstall "Zen Browser"
+WingetSilentInstall Screenbox
 WingetSilentInstall Discord
 WingetSilentInstall VSCode
 WingetSilentInstall Steam
-WingetSilentInstall Bitwarden
-WingetSilentInstall TranslucentTB
 WingetSilentInstall Powershell
+WingetSilentInstall Docker
+
 Refresh-PATH
 
 # Install Rust
 irm -Uri https://win.rustup.rs/x86_64 -Out $Env:TEMP/rustup-init.exe
 &"$Env:TEMP/rustup-init.exe"
+
 Refresh-PATH
 
 # Install Node.js
 fnm install --lts
 fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
 fnm use lts-latest
+
 Refresh-PATH
 
 # Install PowerShell modules
