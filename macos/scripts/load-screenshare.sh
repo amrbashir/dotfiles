a4bd1ce62effb12ba9cedf3cc173ad6a7f8d610e
amrbashir <github@amrbashir.me>
2026-04-14 18:53:39 +0200

    macos: add scripts to load and unload screen sharing service

---
+++ macos/scripts/load-screenshare.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
---
+++ macos/scripts/unload-screenshare.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
